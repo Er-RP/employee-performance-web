c3125724eaaf58eb3ad16ebe675507771a465e75
Rajapandi <92912777+Rajapandi1212@users.noreply.github.com>
2023-03-27 11:25:53 +0530

    project page

---
+++ src/components/projects/index.js
@@ -0,0 +1,9 @@
+import ProjectCard from "./ProjectCard";
+
+const Projects = ({ project }) => {
+  console.log("project card", project);
+
+  return <ProjectCard project={project} />;
+};
+
+export default Projects;
---
+++ src/components/tasks/EmployeeTask.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const EmployeeTask = () => {
+  return <div>employeeTask</div>;
+};
+
+export default EmployeeTask;
